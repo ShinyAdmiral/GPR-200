d20d80d6923f7aaed75f595d2c99a6e0b2d7da9b
Andrew Hunt <andrewshunt6@gmail.com>
2020-11-05 00:53:56 -0500

    final submission

--- fragment.glsl
+++ fragment.glsl
@@ -1,17 +1,30 @@
 #version 450
 
-#ifdef GL_ES
-precision highp float;
-#endif
+//Lighting Code by: Andrew Hunt and Aiden Wright
+//GPR-200-02
+//Assignment: Lab 7
+//Due Date: 5 November 2020
+
+//Sources Used: GLSL STARTER CODE BY DANIEL S. BUCKSTEIN
+//How it was Used: To get us started
+
+#define Verte
+#define Cel
+#define Texture
+#define ViewSpac
 
 layout (location = 0) out vec4 rtFragColor;
-//out vec4 rtFragColor;
 
-
+//------------------------------------------
 //Uniforms
+//------------------------------------------
+
 uniform sampler2D uTextureMap;
 
+//------------------------------------------
 //DATA STRUCTURES
+//------------------------------------------
+
 //data structure for light
 struct pointLight {
 	vec4 center; 	 // center point of light
@@ -19,7 +32,10 @@ struct pointLight {
     float intensity; // intensity of light
 };
 
+//------------------------------------------
 //VARRYING
+//------------------------------------------
+
 //PER-Vertex: recieve the final color
 in vec4 vColor;
 
@@ -36,12 +52,6 @@ in vec4 vTexcoord;
 in pointLight[3] lights;
 
 void main(){
-	
-	//PER-FRAGMENT: calculate final color here using inputs
-	vec4 N = normalize (vNormal);
-	vec4 normalColors = vec4(0.0, 0.0, 0.0, 1.0);//vec4(N.xyz * 0.5 + 0.5, 1.0);
-	rtFragColor = normalColors;
-	
 	// --------------------------------------------------------
 	// CALC LIGHT
 	// --------------------------------------------------------
@@ -69,11 +79,14 @@ void main(){
 	    
 	    vec3 L = normalize(lights[i].center.xyz - vPosition.xyz); // Light Vector
 	    
-	    //vec3 V = normalize(vCamPos.xyz - vPosition.xyz);//aPosition.xyz; // View Vector
-	    //vec3 R = reflect(L, vNormal.xyz);
+	    #ifdef ViewSpace
+	    	vec3 V = normalize(vCamPos.xyz - vPosition.xyz);//aPosition.xyz; // View Vector
+	    	vec3 R = reflect(L, vNormal.xyz);
 	    
-	    vec3 V = vPosition.xyz; // View Vector
-	    vec3 R = reflect(-L, vNormal.xyz);
+	    #else
+	    	vec3 V = vPosition.xyz; // View Vector
+	    	vec3 R = reflect(-L, vNormal.xyz);
+	    #endif
 	    
 	    specularCo = max(0.0, dot(V, R));
 	            
@@ -82,17 +95,27 @@ void main(){
 	    specularIn *= specularIn; //^4
 	    specularIn *= specularIn; //^8
 	    specularIn *= specularIn; //^16
-		
+	    
+	    //calc color with light
 		color += vec3(diffCo * attenIn * lights[i].color + specularIn);
-		//color += vec3(specularIn);
 	}
 	
-	rtFragColor = vec4(color,1.0) * texture(uTextureMap, vec2(1., -1.) * vTexcoord.xy);
-	//rtFragColor = N;
+	#ifdef Cell
+	vec4 lightColor = vec4(color,1.0);
+	float cells = 6.0;
+	float cellColor = lightColor.x * cells;
 	
+	rtFragColor = vec4(0.0, floor(cellColor)/cells, floor(cellColor)/-cells + 1.0, 1.0);
+	#else
+		rtFragColor = vec4(color,1.0);
+	#endif
+	
+	#ifdef Vertex
 	//PER-VERTEX: Input is just final color
-	//rtFragColor = vColor;
+	rtFragColor = vColor;
+	#endif
 	
-	//rtFragColor = vec4(vTexcoord, 0.0, 1.0);
-	//rtFragColor = vTexcoord;
+	#ifdef Texture
+		rtFragColor *= texture(uTextureMap, vec2(1., -1.) * vTexcoord.xy);
+	#endif
 }
--- shaders/lab7VS.glsl
+++ shaders/lab7VS.glsl
@@ -1,19 +1,39 @@
 #version 450
 
+//Lighting Code by: Andrew Hunt and Aiden Wright
+//GPR-200-02
+//Assignment: Lab 7
+//Due Date: 5 November 2020
+
+//Sources Used: GLSL STARTER CODE BY DANIEL S. BUCKSTEIN,
+//http://web.engr.oregonstate.edu/~mjb/cs557/Handouts/morph.1pp.pdf
+//How it was Used: To get us started, and warping vertexs in a box shape.
+
+//for presentation
+#define ViewSpac
+
+#define War
+
+#define Textur
+
+
+//------------------------------------------
 // MAIN DUTY: PROCESS ATTRIBUTES
-// e.g. 3D position in space
-// e.g. normal
-// e.g. 2D uv: texture coordinate
+//------------------------------------------
+
+//3D position in space
 layout (location = 0) in vec4 aPosition;
+
+//normal
 layout (location = 1) in vec3 aNormal;
-//in vec4 aPosition;
 
-// e.g. 2D uv: textuure coordinate
 //Texture Space
-
 layout (location = 2) in vec4 aTexcoord;
 
+//------------------------------------------
 // TRANSFORM UNIFORMS
+//------------------------------------------
+
 uniform mat4 uModelMat;
 uniform mat4 uViewMat;
 uniform mat4 uProjMat;
@@ -21,7 +41,10 @@ uniform mat4 uViewProjMat;
 uniform sampler2D uTextureMap;
 uniform float iTime;
 
+//------------------------------------------
 //DATA STRUCTURES
+//------------------------------------------
+
 //data structure for light
 struct pointLight {
 	vec4 center; 	 // center point of light
@@ -29,8 +52,9 @@ struct pointLight {
     float intensity; // intensity of light
 };
 
-
+//------------------------------------------
 //VARRYING
+//------------------------------------------
 
 //PER-Vertex: send final color
 out vec4 vColor;
@@ -47,29 +71,9 @@ out vec4 vTexcoord;
 out pointLight[3] lights;
 
 void main(){
-	//vPosition = aPosition;
-	
-	// REQUIRED: set this value:
-	// problem: gl_positionin in "clip-sapce"
-	// problem: aPositin is in "object-space"
-	//gl_Position = aPosition;
-	
-	//position in world-space (wrong)
+	//position in world-space
 	vec4 pos_world = uModelMat * aPosition;
 	vPosition = pos_world;
-	
-	//gl_Position = pos_world;
-	
-	// position camera-space (also wrong))
-	//vec4 pos_camera = uViewMat * uModelMat * aPosion;
-	//vec4 pos_camera = uViewMat * pos_world;
-	//gl_Position = pos_camera;
-	
-	//Position in clip space
-	//vec4 pos_clip = uViewProjMat * pos_world;
-	//vec4 pos_clip = uViewProjMat * uViewMat * uModelMat * aPosition;
-	//vec4 pos_clip = uViewProjMat * pos_camera;
-	//gl_Position = pos_clip;
 	
 	// POSITION PIPELINE
 	mat4 modelViewMat = uViewMat * uModelMat;
@@ -84,16 +88,14 @@ void main(){
 	
 	//TEXCOORD PIPELINE
 	//for drawing parts of the texture                                          
-	mat4 atlastMat = mat4( 0.5, 0.0, 0.0, 0.0,
-						   0.0, 0.5, 0.0, 0.0,
-						   0.0, 0.0, 1.0, 0.0,
-						   0.25, 0.25, 0.0, 1.0);
+	mat4 atlastMat = mat4(0.5, 0.0, 0.0, 0.0,
+						 0.0, 0.5, 0.0, 0.0,
+						 0.0, 0.0, 1.0, 0.0,
+						 0.25,0.25,0.0,1.0);
 	vec4 uv_atlas = atlastMat * aTexcoord;
 	
 	//PER-VERTEX: pass things that FS need to calc
 	vNormal = vec4(norm_camera, 0.0);
-	
-	
 	
 	// --------------------------------------------------------
 	// CALC LIGHT
@@ -110,14 +112,16 @@ void main(){
 	lights[2].color 	= vec4(1.0);
 	lights[2].intensity = 3.0;
 	
-	
+	//starting color
 	vec3 color = vec3(0.0);
 	
+	//determine normal
+	#ifdef ViewSpace
+	vec3 lightForNorm = norm_camera;
 	
-	vec3 lightForNorm;
-	//lightForNorm = norm_camera;
-	lightForNorm = aNormal;
-	
+	#else
+	vec3 lightForNorm = aNormal;
+	#endif
 	
 	for (int i = 0; i < 3; i ++){
 	
@@ -137,11 +141,14 @@ void main(){
 	    
 	    vec3 L = normalize(lights[i].center.xyz - aPosition.xyz); // Light Vector
 	    
-	    //vec3 V = normalize(pos_camera.xyz - aPosition.xyz);//aPosition.xyz; // View Vector
-	   	//vec3 R = reflect(L, lightForNorm);
+	    #ifdef ViewSpace
+	    vec3 V = normalize(pos_camera.xyz - aPosition.xyz);// View Vector
+	   	vec3 R = reflect(L, lightForNorm);
 	    
+	    #else
 	    vec3 V = aPosition.xyz; // View Vector
 	    vec3 R = reflect(-L, lightForNorm);
+	    #endif
 	    
 	    specularCo = max(0.0, dot(V, R));
 	            
@@ -154,33 +161,46 @@ void main(){
 		
 		color += vec3(diffCo * attenIn * lights[i].color + specularIn);
 	}
-	//	vec4 uv_atlas = atlasMat * aTexcoord * texture(uTextureMap);
 	
-	vColor = vec4(color,1.0) * texture(uTextureMap, aTexcoord.xy);
+	#ifdef Texture
+		vColor = vec4(color,1.0) * texture(uTextureMap, aTexcoord.xy);
+
+	
+	#else
+		vColor = vec4(color,1.0);
+	
+	#endif
+	
+	#ifdef ViewSpace
+	vNormal = vec4(norm_camera, 1.0);
+	
+	#else
 	vNormal = vec4(aNormal, 1.0);
+	#endif
 	
-	
-	//Optional: set varyings
-	//vColor = vec4 (1.0, 0.5, 0.0, 1.0);
-	//vColor = aPosition * 0.5 + 0.5;
-	
+	//texture coord
 	vTexcoord = aTexcoord;
 	
-	/*
+	#ifdef Warp
 	//Credit: http://web.engr.oregonstate.edu/~mjb/cs557/Handouts/morph.1pp.pdf
 	
 	//get side count in relation to time
-	const float SIDE = sin(iTime) * 0.5 + 2.0;
+	float side = sin(iTime) * 0.5 + 2.0;
 	
 	//start with object in clip space
 	vec4 vertex = pos_clip;
 	
 	//box it 
 	vertex.xyz *= 4. / length(vertex.xyz);
-	vertex.xyz = clamp( vertex.xyz, -SIDE, SIDE );
+	vertex.xyz = clamp( vertex.xyz, -side, side);
 	
 	gl_Position = vertex;//pos_clip * mod(iTime, 10.0);
-	*/
+	
+	#else
+	
+	vTexcoord = aTexcoord;
 	
 	gl_Position = pos_clip;
+	
+	#endif
 }
