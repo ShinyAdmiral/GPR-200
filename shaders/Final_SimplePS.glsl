9ae15c4b07ef036fedcdebc9a90eac9ec5df7fe5
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2020-12-07 11:43:44 -0800

    texture issue

--- shaders/Final_SimplePS.glsl
+++ shaders/Final_SimplePS.glsl
@@ -2,7 +2,28 @@
 
 in vec4 color;
 out vec4 outColor;
-
+in vec2 bWater;
 void main() {
-   outColor = vec4(color);
+
+if(true)
+{
+	outColor = color;
+	return;
+}
+	if(bWater.x > 0.)
+	{
+		outColor =	vec4(0.,0.,1.,.25);
+		return;
+	}
+	
+	float angle = dot(vec3(0,0,1.), color.xyz);
+	if(angle > .5)
+	{
+		outColor = vec4(0.,angle,0., 1.);
+		return;
+	}
+	else
+	{
+		outColor = vec4(angle);
+	}
 }
--- shaders/Final_SimpleVS.glsl
+++ shaders/Final_SimpleVS.glsl
@@ -4,52 +4,66 @@ uniform mat4 matVP;
 uniform mat4 matGeo;
 uniform float matMaxHeight;
 uniform sampler2D noiseTex;
+uniform sampler2D normTex;
 uniform float iTime;
 uniform bool water;
 layout (location = 0) in vec3 pos;
 layout (location = 1) in vec3 normal;
 layout (location = 2) in vec2 texCoord;
 out vec4 color;
-
+out vec2 bWater;
 void main() 
 {
-	vec2 lookUp = texCoord + .2;
+	if(water)
+	{
+	bWater = vec2(1.,1.);
+	}
+	else
+	{
+	bWater = vec2(0.,0.);
+	}
+
+	vec2 lookUp = texCoord;
 	float timeOff = iTime;
 	timeOff = 1.2;
-	lookUp.y += timeOff / 1.5;
-	lookUp *= .1;					
+	float scale = .85;
+	lookUp *= scale; 				
 	
 	float origin = texture(noiseTex, lookUp).r;
-	vec2 off = vec2(0.01,0.01);
-	float top = texture(noiseTex, lookUp + off.y).r;
+
+	float dif = 0.01;
+	vec2 off = vec2(dif,dif);
+	float top =    texture(noiseTex, lookUp + off.y).r;
 	float bottom = texture(noiseTex, lookUp - off.y).r;
-	float left = texture(noiseTex, lookUp + off.x).r;
-	float right = texture(noiseTex, lookUp - off.x).r;
-	
-	
-	
+	float left =   texture(noiseTex, lookUp + off.x).r;
+	float right =  texture(noiseTex, lookUp - off.x).r;
+				
 	vec4 newPos = vec4(pos, 1.);
 	float maxHeight = matMaxHeight;
 	
-//	maxHeight *= .8;
+	maxHeight *= .2;
 	newPos.x += origin * maxHeight;
+	
 	vec3 newNorm = vec3(0.0);
-	
+
 	newNorm = vec3(origin);
-	
-	// rise and run
-	// difference of horizontal change (change over x & y)
-	// cross product of that
 	
 	// uv & sample
 	vec3 deltaHoriz = vec3(off.x + off.x, 0., right - left);	
-	vec3 deltaVertical = vec3(0., off.y + off.y, top - bottom);
+	vec3 deltaVertical = vec3(0., off.y + off.y,  top - bottom);
 	deltaHoriz = normalize(deltaHoriz);
 	deltaVertical = normalize(deltaVertical);
 	newNorm = cross(deltaHoriz, deltaVertical);
 	
-	color = vec4(newNorm, 0.5); // alpha blending
+	newNorm = texture(normTex, lookUp).xyz;
+
+	color = vec4(newNorm, 1.0); // alpha blending
 	if(water)
-	color = vec4(0.,0.,1.,0.);
+	{
+		color = vec4(0.,0.,1.,0.);
+	}
+
+
 	gl_Position = matVP * matGeo * newPos;
+
 }
